c9b2b307c951922c49e07b8d679f75a4ce8518c0
haym8 <31711187+haym8@users.noreply.github.com>
2018-04-16 00:08:07 -0400

    Update keithley2.py

--- keithley2.py
+++ keithley2.py
@@ -1,58 +1,54 @@
 import pyvisa
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+import csv
 
-rm = pyvisa.ResourceManager()
-k = rm.open_resource("ASRL7::INSTR")
-k.baud_rate = 19200
-k.data_bits = 8
-k.parity = pyvisa.constants.Parity.none
-k.stop_bits = pyvisa.constants.StopBits.one
-k.read_termination = '\r'
-k.timeout = 50000
+class Keithley2:
+    def __init__(self):
+        rm = pyvisa.ResourceManager()
+        k = rm.open_resource("ASRL7::INSTR")
+        k.baud_rate = 19200
+        k.data_bits = 8
+        k.parity = pyvisa.constants.Parity.none
+        k.stop_bits = pyvisa.constants.StopBits.one
+        k.read_termination = '\r'
+        k.timeout = 50000
+        voltages = []
+        volt_form = []
+        
+    def ready(self):
+        k.write("*RST")
+        k.write(":FUNC 'VOLT:DC'")
+        k.write(":SENS:VOLT:DC:AVER:STAT OFF")
+        k.write(":SENS:VOLT:DC:NPLC 0.01")
+        k.write(":SENS:VOLT:DC:DIG 4")
+        k.write(":RES:RANGE 1")
+        k.write(":RES:AVER:STAT OFF")
+        k.write(":FORM:ELEM READ")
+        k.write(":TRIG:COUN 1")
+        k.write(":TRIG:DEL 0.0")
+        k.write(":TRIG:SOUR IMM")
+        k.write(":SYSTEM:AZERO:STAT OFF")
+        k.write(":DISP:ENAB OFF")
+        k.write(":SAMP:COUNT 1")
+    def read(self, running)
+        while(running):
+            voltages.append(k.query(":READ?"))
 
-voltages = []
-volt_form = []
-times = []
+    def convert(self):
+        for j in range(len(voltages)):
+            v = voltages[j].encode("ascii")
+            vn = v.split(",")
+            for elem in vn:
+                volt_form.append(float(elem))
 
-
-k.write("*RST")
-k.write(":FUNC 'VOLT:DC'")
-k.write(":SENS:VOLT:DC:AVER:STAT OFF")
-k.write(":SENS:VOLT:DC:NPLC 0.01")
-k.write(":SENS:VOLT:DC:DIG 4")
-k.write(":RES:RANGE 1")
-k.write(":RES:AVER:STAT OFF")
-k.write(":FORM:ELEM READ")
-k.write(":TRIG:COUN 1")
-k.write(":TRIG:DEL 0.0")
-k.write(":TRIG:SOUR IMM")
-k.write(":SYSTEM:AZERO:STAT OFF")
-k.write(":DISP:ENAB OFF")
-
-for j in range(1,10):
-    k.write(":SAMP:COUNT " + str(j))
-    for i in range(1000/j):
-        a = time.time()
-        voltages.append(k.query(":READ?"))
-        b = time.time() - a
-        times.append(b)
-
-    print "Sample Count: " + str(j)
-    print "Average Time Off: " + str(np.mean(times))
-    print "Total Time: " + str(np.sum(times))
-    print "Sampling Rate: " + str(1000/np.sum(times))
-
-    times = []
-    
-for j in range(len(voltages)):
-    v = voltages[j].encode("ascii")
-    vn = v.split(",")
-    for elem in vn:
-        volt_form.append(float(elem))
-
-k.close()
+    def writeToCSV(self, fileLoc):
+        with open(fileLoc, "wb") as csvfile:
+            writeOut = csv.writer(csvfile, delimiter=',')
+            writeOut.writerow(volt_form)
+                
+    def close(self):
+        k.close()
 
 #plt.plot(volt_form)
 #plt.show()

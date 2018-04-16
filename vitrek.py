647a997d81998788c0de97789eb5c7db42926f2f
haym8 <31711187+haym8@users.noreply.github.com>
2018-04-16 17:30:52 -0400

    Update vitrek.py

--- vitrek.py
+++ vitrek.py
@@ -1,22 +1,30 @@
 import pyvisa
 
 class Vitrek:
-  
-  def __init__(self, testType, vMax):
-    self.testType = testType
-    self.vMax = vMax
-    
-   
-rm = pyvisa.ResourceManager()
-v = rm.open_resource("ASRL10::INSTR")
-v.baud_rate = 9600
-v.data_bits = 8
-v.parity = pyvisa.constants.Parity.none
-v.stop_bits = pyvisa.constants.StopBits.one
-#v.read_termination = '\r'
-v.timeout = 5000
+    def __init__(self):
+        rm = pyvisa.ResourceManager()
+        v = rm.open_resource("ASRL10::INSTR")
+        v.baud_rate = 9600
+        v.data_bits = 8
+        v.parity = pyvisa.constants.Parity.none
+        v.stop_bits = pyvisa.constants.StopBits.one
+        v.read_termination = '\r\n'
+        v.timeout = 5000
 
-v.write("NOSEQ;ADD,ACEZ,100.0,60.0,1.5,3.0,0.0,0.005,ABORT;RUN")
-v.query("RUN?")
+    def sendTest(self, testType, voltType, voltMax):
+        if voltType = "DC":
+            voltType = "DCEZ"
+        elif voltType = "AC":
+            voltType = "ACEZ"
+            
+        v.write("NOSEQ;ADD," +
+                voltType +
+                "," + str(voltMax)  +
+                ",60.0," + (voltMax/500.0) +
+                ",3.0,0.0,0.005,ABORT;RUN")
 
-v.close()
+    def getPass(self):
+        res = v.query("STEPRSLT?,1")
+        
+    def close(self):
+        v.close()

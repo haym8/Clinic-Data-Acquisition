1fff84750365062b621c58d6410d2a3ff5bb99a0
haym8 <31711187+haym8@users.noreply.github.com>
2018-05-04 17:24:49 -0400

    Update vitrek.py

--- vitrek.py
+++ vitrek.py
@@ -3,15 +3,16 @@ import pyvisa
 class Vitrek:
     def __init__(self):
         self.rm = pyvisa.ResourceManager()
-        self.v = self.rm.open_resource("ASRL10::INSTR", access_mode=4)
+        self.v = self.rm.open_resource("ASRL10::INSTR")
         self.v.baud_rate = 9600
         self.v.data_bits = 8
         self.v.parity = pyvisa.constants.Parity.none
         self.v.stop_bits = pyvisa.constants.StopBits.one
-        self.v.read_termination = '\r'
+        #self.v.read_termination = '\r\n'
+        self.v.write_termination = '\r\n'
         self.v.set_visa_attribute(pyvisa.constants.VI_ATTR_ASRL_FLOW_CNTRL,
                                   pyvisa.constants.VI_ASRL_FLOW_RTS_CTS)
-        self.v.timeout = 20000
+        self.v.timeout = 5000
 
     def sendTest(self, testType, voltType, voltMax):
         stringSend = ""
@@ -22,6 +23,12 @@ class Vitrek:
             
         self.v.write(stringSend)
 
+    def getRunning(self):
+        if self.v.query("RUN?"):
+            return True
+        else:
+            return False
+        
     def getResults(self):
         res = self.v.query("STEPRSLT?,1")
         return res
